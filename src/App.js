55615662d19b7cae0625c0048a17873345d4fd97
seoyoung park <ongddree@gmail.com>
2022-01-22 00:12:42 +0900

    ♻️  PublicRoutes, PrivateRoutes 추상화

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import login from './pages/Login';
 import loginEmail from './pages/Login/email';
 import join from './pages/Join';
@@ -9,21 +8,22 @@ import joinSetting from './pages/Join/setting';
 import home from './pages/Home';
 import profile from './pages/Profile';
 import otherProfile from './pages/Profile/[id]';
+import PrivateRoute from './pages/Login/components/PrivateRoutes';
+import PublicRoute from './pages/Login/components/PublicRoute';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route path="/home" exact component={home} />
-        <Route path="/login" exact component={login} />
-        <Route path="/login/email" exact component={loginEmail} />
-        <Route path="/join" exact component={join} />
-        <Route path="/join/setting" exact component={joinSetting} />
-        <Route path="/join/setting" exact component={joinSetting} />
-
-        {/* profile */}
-        <Route path="/profile" exact component={profile} />
-        <Route path="/profile/:id" exact component={otherProfile} />
+        <Switch>
+          <PublicRoute path="/login" exact component={login} />
+          <PublicRoute path="/login/email" exact component={loginEmail} />
+          <PublicRoute path="/join" exact component={join} />
+          <PublicRoute path="/join/setting" exact component={joinSetting} />
+          <PrivateRoute path="/home" exact component={home} />
+          <PrivateRoute path="/profile" exact component={profile} />
+          <PrivateRoute path="/profile/:id" exact component={otherProfile} />
+        </Switch>
       </BrowserRouter>
     </div>
   );
