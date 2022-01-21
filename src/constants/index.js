8653a729f346cef91cbe878e3fac667639bedb4c
seoyoung park <ongddree@gmail.com>
2022-01-21 20:13:38 +0900

    ✨ API ENDPOINT 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,35 +1,35 @@
-export const API_ENDPOINT = "";
+export const API_ENDPOINT = 'http://146.56.183.55:5050/';
 
 export const ROUTES = [
   {
     ID: 0,
-    PATH: "/home",
-    LABEL: "홈",
+    PATH: '/home',
+    LABEL: '홈',
   },
   {
     ID: 1,
-    PATH: "/chat",
-    LABEL: "채팅",
+    PATH: '/chat',
+    LABEL: '채팅',
   },
   {
     ID: 2,
-    PATH: "/post",
-    LABEL: "게시물 작성",
+    PATH: '/post',
+    LABEL: '게시물 작성',
   },
   {
     ID: 3,
-    PATH: "/profile",
-    LABEL: "프로필",
+    PATH: '/profile',
+    LABEL: '프로필',
   },
 ];
 
 export const PALLETS = {
-  CREAM: "#f2eddc",
-  BEIGE: "#f2c894" /*비활성화*/,
-  ORANGE: "#d98c5f" /*활성화*/,
-  BROWN: "#734743" /*스플래시*/,
-  BLACK: "#000000",
-  WHITE: "#ffffff",
-  LIGHTGRAY: "#d0d3d9",
-  GRAY: "#767676" /*버튼 글씨*/,
+  CREAM: '#f2eddc',
+  BEIGE: '#f2c894' /*비활성화*/,
+  ORANGE: '#d98c5f' /*활성화*/,
+  BROWN: '#734743' /*스플래시*/,
+  BLACK: '#000000',
+  WHITE: '#ffffff',
+  LIGHTGRAY: '#d0d3d9',
+  GRAY: '#767676' /*버튼 글씨*/,
 };
