00b77df86a522bd0cecc9a20ef1dc3c93724ed56
yoojin-park19 <yoojin3972@gmail.com>
2022-01-23 00:57:15 +0900

    :sparkles: 홈피드 보여주기 기능 구현

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,46 +1,39 @@
-import styled from '@emotion/styled';
-import { PALLETS } from '../../constants';
-import { Link } from 'react-router-dom';
 import { Searchhead } from '../layouts/Searchhead';
 import { Tabmenu } from '../layouts/Tabmenu';
+import { useState, useEffect } from 'react';
+import { Searchuser } from './Components/Searchuser';
+import { Userfeed } from './Components/Userfeed';
 
 const HomeIndexPage = () => {
+  const now = new Date();
+  const [isLike, setIsLike] = useState(false);
+  const toggleLike = () => {
+    isLike ? setIsLike(false) : setIsLike(true);
+  };
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    fetch('http://146.56.183.55:5050/post/feed', {
+      method: 'GET',
+      headers: {
+        // localStorage.getItem('token') 으로 현재 사용자(본인)의 토큰 받아오기
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZWEwOTQ3NDU4ZjFkZGQyZTJiYTE5MiIsImV4cCI6MTY0NzkyNzEwOSwiaWF0IjoxNjQyNzQzMTA5fQ.kxZJWVlF8-1DFZKxflFBAmEr6jRyq2ynCRIiTTGP6Ks`,
+        'Content-type': 'application/json',
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setPost(data.posts);
+      });
+  }, []);
   return (
     <>
       <Searchhead />
-      <Searchuser>
-        <img src="/assets/logo.png" alt="logo" />
-        <p>유저를 검색해 팔로우 해보세요!</p>
-        <Link to="/home/search">검색하기</Link>
-      </Searchuser>
-      <Tabmenu />
+      {post.length ? <Userfeed /> : <Searchuser />}
+      <Tabmenu route={'홈'}></Tabmenu>
     </>
   );
 };
 
 export default HomeIndexPage;
-
-const Searchuser = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  font-size: 14px;
-  height: 712px;
-  img {
-    width: 100px;
-    margin-bottom: 20px;
-  }
-  a {
-    height: 30px;
-    padding: 8px 40px;
-    border-radius: 30px;
-    background-color: ${PALLETS.ORANGE};
-    color: #fff;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 18px;
-    text-align: center;
-    margin-top: 18px;
-  }
-`;
--- src/pages/Home/search.jsx
+++ src/pages/Home/search.jsx
@@ -1,15 +1,7 @@
-import styled from '@emotion/styled';
-import Link from 'next/link';
-import { Mainform } from '../../components/layouts/Mainform';
-import { PALLETS } from '../../constants';
-import { Searchuserhead } from '../../components/layouts/Searchuserhead';
+import { Searchuserhead } from '../layouts/Searchuserhead';
 
 const SearchPage = () => {
-  return (
-    <Mainform>
-      <Searchuserhead />
-    </Mainform>
-  );
+  return <Searchuserhead />;
 };
 
 export default SearchPage;
