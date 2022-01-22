07aa904d18184bf38bba5f8a34879c3ff683afe3
seoyoung park <ongddree@gmail.com>
2022-01-22 17:00:25 +0900

    ✨  탭메뉴 활성화 추가

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,21 +5,29 @@ export const ROUTES = [
     ID: 0,
     PATH: '/home',
     LABEL: '홈',
+    INACTIVE: '/assets/icon/icon-home.svg',
+    ACTIVE: '/assets/icon/icon-home-fill.svg',
   },
   {
     ID: 1,
     PATH: '/chat',
     LABEL: '채팅',
+    INACTIVE: '/assets/icon/icon-message-circle.svg',
+    ACTIVE: '/assets/icon/icon-message-circle-fill.svg',
   },
   {
     ID: 2,
     PATH: '/post',
     LABEL: '게시물 작성',
+    INACTIVE: '/assets/icon/icon-edit.svg',
+    ACTIVE: '/assets/icon/icon-edit-fill.svg',
   },
   {
     ID: 3,
     PATH: '/profile',
     LABEL: '프로필',
+    INACTIVE: '/assets/icon/icon-user.svg',
+    ACTIVE: '/assets/icon/icon-user-fill.svg',
   },
 ];
 
--- src/pages/layouts/Tabmenu.jsx
+++ src/pages/layouts/Tabmenu.jsx
@@ -2,18 +2,29 @@ import { Link } from 'react-router-dom';
 import { PALLETS, ROUTES } from '../../constants';
 import styled from '@emotion/styled';
 
-export const Tabmenu = () => {
+export const Tabmenu = (route) => {
   return (
     <nav>
       <Tabmenus>
         {ROUTES.map((routeObject) => {
-          return (
-            <li key={`tab-${routeObject.ID}`}>
-              <Link to={routeObject.PATH}>
-                <Menubtn>{routeObject.LABEL}</Menubtn>
-              </Link>
-            </li>
-          );
+          if (routeObject.LABEL === route.route) {
+            return (
+              <li key={`tab-${routeObject.ID}`}>
+                <Tablink to={routeObject.PATH}>
+                  <Icon src={routeObject.ACTIVE}></Icon>
+                  <Menubtn>{routeObject.LABEL}</Menubtn>
+                </Tablink>
+              </li>
+            );
+          } else
+            return (
+              <li key={`tab-${routeObject.ID}`}>
+                <Tablink to={routeObject.PATH}>
+                  <Icon src={routeObject.INACTIVE}></Icon>
+                  <Menubtn>{routeObject.LABEL}</Menubtn>
+                </Tablink>
+              </li>
+            );
         })}
       </Tabmenus>
     </nav>
@@ -21,21 +32,39 @@ export const Tabmenu = () => {
 };
 
 const Tabmenus = styled.ul`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
   display: flex;
+  justify-content: space-between;
   gap: 14px;
   padding: 0 6px;
   height: 60px;
-  background: ${PALLETS.BEIGE};
+  border-top: 0.5px solid ${PALLETS.LIGHTGRAY};
 `;
 
-const Menubtn = styled.a`
+const Tablink = styled(Link)`
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+const Menubtn = styled.strong`
   position: relative;
   font-size: 10px;
   color: ${PALLETS.GRAY};
   line-height: 14px;
-  padding: 39px 0px 6px;
+  padding: 0px 6px;
   width: 84px;
+`;
+
+const Icon = styled.img`
+  display: block;
+  content: '';
+  width: 24px;
+  height: 24px;
+  margin-bottom: 5px;
 `;
