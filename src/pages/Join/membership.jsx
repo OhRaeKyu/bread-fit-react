f0affacafe1a8d9e484fa472acdaa9f0e31dd9b9
seoyoung park <ongddree@gmail.com>
2022-01-24 22:58:06 +0900

    ✨ 회원가입 컴포넌트 분리 (이메일 가입/프로필 설정)

---
+++ src/pages/Join/membership.jsx
@@ -0,0 +1,189 @@
+import styled from '@emotion/styled';
+import { PALLETS } from '../../constants';
+import { useState } from 'react';
+import { API_ENDPOINT } from '../../constants';
+import { useHistory } from 'react-router-dom';
+import { Tabmenu } from '../layouts/Tabmenu';
+
+const MembershipPage = ({ setMode, handleUserdata }) => {
+  const [error, setError] = useState('');
+  const [email, setEmail] = useState('');
+  const [pw, setPw] = useState('');
+
+  const checkEmail =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+  const handleIdInput = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePwInput = (event) => {
+    setPw(event.target.value);
+  };
+
+  const checkJoin = () => {
+    if (pw.length < 6) {
+      setError('pw');
+      return;
+    } else submitJoin();
+  };
+
+  const submitJoin = async () => {
+    try {
+      const res = await fetch(`${API_ENDPOINT}` + 'user/emailvalid', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            email: email,
+          },
+        }),
+      });
+      const json = await res.json();
+      console.log(json);
+      if (json.message === '이미 가입된 이메일 주소 입니다.') {
+        setError('email');
+        console.log(json.message);
+      } else {
+        handleUserdata('email', email);
+        handleUserdata('password', pw);
+        setMode(false);
+        console.log(json.message);
+      }
+    } catch (err) {
+      setError('email');
+    }
+  };
+
+  return (
+    <>
+      <Form method="POST">
+        <Formtit>이메일로 회원가입</Formtit>
+        <Fieldset>
+          <Label htmlFor="id">아이디</Label>
+          <Input
+            required
+            id="id"
+            type="email"
+            placeholder="이메일 주소를 입력해 주세요."
+            onChange={handleIdInput}
+          />
+          <Error display={error}>*이미 가입한 이메일 주소입니다.</Error>
+        </Fieldset>
+        <Fieldset>
+          <Label htmlFor="pw">비밀번호</Label>
+          <Input
+            required
+            id="password"
+            type="password"
+            placeholder="비밀번호를 설정해 주세요."
+            onChange={handlePwInput}
+            autoComplete="off"
+          />
+          <Errorpw display={error}>*비밀번호는 6자 이상이어야 합니다.</Errorpw>
+        </Fieldset>
+        <Btnsubmit
+          type="button"
+          disabled={!(checkEmail.test(email) && pw.length > 2)}
+          onClick={checkJoin}
+        >
+          다음
+        </Btnsubmit>
+      </Form>
+      <Tabmenu route={'게시물 작성'} />
+    </>
+  );
+};
+
+export default MembershipPage;
+
+const Form = styled.form`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 322px;
+  padding: 50px 0 0;
+`;
+
+const Fieldset = styled.fieldset`
+  & ~ & {
+    margin-top: 16px;
+  }
+`;
+
+const Formtit = styled.h2`
+  margin-bottom: 40px;
+  text-align: center;
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: 500;
+  color: black;
+`;
+
+const Label = styled.label`
+  font-size: 12px;
+  line-height: 15px;
+  font-weight: 500;
+  color: ${PALLETS.GRAY};
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  height: 32px;
+  border: 1px solid ${PALLETS.LIGHTGRAY};
+  border-left-width: 0;
+  border-right-width: 0;
+  border-top-width: 0;
+  border-bottom-width: 1;
+  font-size: 14px;
+
+  ::placeholder {
+    color: ${PALLETS.LIGHTGRAY};
+  }
+
+  &:focus {
+    outline: none;
+    border-color: ${PALLETS.ORANGE};
+  }
+`;
+
+const Error = styled.strong`
+  margin-top: 6px;
+  display: ${(props) => (props.display === 'email' ? 'block' : 'none')};
+  color: #eb5757;
+  font-size: 12px;
+  line-height: 14px;
+`;
+
+const Errorpw = styled.strong`
+  margin-top: 6px;
+  display: ${(props) => (props.display === 'pw' ? 'block' : 'none')};
+  color: #eb5757;
+  font-size: 12px;
+  line-height: 14px;
+`;
+
+const Btnsubmit = styled.button`
+  margin-top: 30px;
+  white-space: nowrap;
+  padding: 0px 107px;
+  height: 44px;
+  width: 322px;
+  padding: 13px 0;
+  text-align: center;
+  border-radius: 44px;
+  background-color: ${PALLETS.BROWN};
+  color: ${PALLETS.WHITE};
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 17.53px;
+  text-align: center;
+
+  &:disabled {
+    background-color: ${PALLETS.BEIGE};
+    cursor: not-allowed;
+  }
+`;
--- src/pages/Join/setting.jsx
+++ src/pages/Join/setting.jsx
@@ -1,42 +1,152 @@
 import styled from '@emotion/styled';
 import { PALLETS } from '../../constants';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { API_ENDPOINT } from '../../constants';
 
-const SettingPage = () => {
+const SettingPage = ({ userdata, handleUserdata, submitUserdata }) => {
+  const [error, setError] = useState('');
+  const [previewSrc, setPreviewSrc] = useState('/assets/logo-white.svg');
+  const [imagefile, setImagefile] = useState(null);
+
+  //username 관리
+  const handleNameInput = (event) => {
+    handleUserdata('name', event.target.value);
+  };
+
+  //account name 관리
+  const HandleAccountInput = (event) => {
+    handleUserdata('accountname', event.target.value);
+  };
+
+  //intro 관리
+  const HandleIntroInput = (event) => {
+    handleUserdata('intro', event.target.value);
+  };
+
+  //이미지 프리뷰
+  const handleImgInput = (e) => {
+    setImagefile(e.target.files);
+    encodeFileToBase64(e.target.files[0]);
+  };
+
+  const encodeFileToBase64 = (fileBlob) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(fileBlob);
+    return new Promise((resolve) => {
+      reader.onload = () => {
+        setPreviewSrc(reader.result);
+        resolve();
+      };
+    });
+  };
+
+  //이미지 업로드
+  async function imageUpload(files) {
+    const formData = new FormData();
+    formData.append('image', files[0]);
+    const res = await fetch(`http://146.56.183.55:5050/image/uploadfile`, {
+      method: 'POST',
+      body: formData,
+    });
+    const data = await res.json();
+    const ImgName = `http://146.56.183.55:5050/${data['filename']}`;
+    console.log(ImgName);
+    return ImgName;
+  }
+
+  //accountname 관리 - 유효성 검사
+  const checkAvailable = /^[0-9a-zA-Z_.]+$/;
+
+  const checkAccount = () => {
+    if (!checkAvailable.test(userdata.accountname)) {
+      setError('unavailable');
+      return;
+    } else submitAccount();
+  };
+
+  //accountname 관리 - 중복검사
+  const submitAccount = async () => {
+    try {
+      const imgurl = await imageUpload(imagefile);
+      const res = await fetch(`${API_ENDPOINT}` + 'user/accountnamevalid', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: {
+            accountname: userdata.accountname,
+          },
+        }),
+      });
+      const json = await res.json();
+      if (json.message === '사용 가능한 계정ID 입니다.') {
+        handleUserdata('image', imgurl);
+        submitUserdata();
+      } else {
+        setError('duplication');
+      }
+    } catch (err) {
+      setError('email');
+    }
+  };
+
   return (
     <Form method="POST">
-      <h2>프로필 설정</h2>
-      <p>나중에 언제든지 변경할 수 있습니다.</p>
+      <Formtit>프로필 설정</Formtit>
+      <Formdesc>나중에 언제든지 변경할 수 있습니다.</Formdesc>
       <section>
-        <img src="/assets/logo-white.svg" alt="" />
-        <label htmlFor="upload">
-          <input type="file" id="upload" className="sr-only" accept="image/*" />
-        </label>
+        <img src={previewSrc} alt="" />
+        <Label htmlFor="upload">
+          <input
+            type="file"
+            id="upload"
+            className="sr-only"
+            accept="image/*"
+            onChange={handleImgInput}
+          />
+        </Label>
       </section>
-      <fieldset>
-        <label htmlfor="name">사용자 이름</label>
-        <input id="name" type="text" placeholder="2~10자 이내여야 합니다." />
-      </fieldset>
-      <fieldset>
-        <label htmlfor="id">계정 ID</label>
-        <input
+      <Fieldset>
+        <Label htmlfor="name">사용자 이름</Label>
+        <Input
+          id="name"
+          type="text"
+          placeholder="2~10자 이내여야 합니다."
+          minLength={2}
+          maxLength={10}
+          onChange={handleNameInput}
+        />
+      </Fieldset>
+      <Fieldset>
+        <Label htmlfor="id">계정 ID</Label>
+        <Input
           id="id"
           type="text"
           placeholder="영문, 숫자, 특수문자(.),(_)만 사용 가능합니다."
+          onChange={HandleAccountInput}
         />
-        <strong>*비밀번호가 일치하지않습니다.</strong>
-      </fieldset>
-      <fieldset>
-        <label htmlfor="desc">소개</label>
-        <input
+        <Error display={error}>
+          *영문, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.
+        </Error>
+        <Errorid display={error}>*이미 가입된 계정 ID입니다.</Errorid>
+      </Fieldset>
+      <Fieldset>
+        <Label htmlfor="desc">소개</Label>
+        <Input
           id="desc"
           type="text"
           placeholder="자신과 판매할 상품에 대해 소개해 주세요!"
+          onChange={HandleIntroInput}
         />
-      </fieldset>
-      <Link to="/home" passHref>
-        <button type="submit">감귤마켓 시작하기</button>
-      </Link>
+      </Fieldset>
+      <Btnsubmit
+        type="button"
+        onClick={checkAccount}
+        disabled={!(userdata.name && userdata.accountname && userdata.intro)}
+      >
+        감귤마켓 시작하기
+      </Btnsubmit>
     </Form>
   );
 };
@@ -44,26 +154,11 @@ const SettingPage = () => {
 export default SettingPage;
 
 const Form = styled.form`
-  width: 390px;
-  height: 844px;
-  padding: 54px 34px 499px;
-
-  h2 {
-    margin-bottom: 12px;
-    text-align: center;
-    font-size: 24px;
-    line-height: 30px;
-    font-weight: 500;
-    color: black;
-  }
-
-  p {
-    font-size: 14px;
-    line-height: 14px;
-    color: ${PALLETS.GRAY};
-    text-align: center;
-    margin-bottom: 30px;
-  }
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 322px;
+  padding: 50px 0 0;
 
   section {
     position: relative;
@@ -86,65 +181,94 @@ const Form = styled.form`
       right: 0;
     }
   }
+`;
 
-  fieldset + fieldset {
+const Formtit = styled.h2`
+  margin-bottom: 12px;
+  text-align: center;
+  font-size: 24px;
+  line-height: 30px;
+  font-weight: 500;
+  color: black;
+`;
+
+const Formdesc = styled.p`
+  font-size: 14px;
+  line-height: 14px;
+  color: ${PALLETS.GRAY};
+  text-align: center;
+  margin-bottom: 30px;
+`;
+
+const Fieldset = styled.fieldset`
+  & ~ & {
     margin-top: 16px;
   }
+`;
 
-  label {
-    font-size: 12px;
-    line-height: 15px;
-    font-weight: 500;
-    color: var(--bg-color-8);
+const Label = styled.label`
+  // font-size: 12px;
+  // line-height: 15px;
+  // font-weight: 500;
+  // color: ${PALLETS.GRAY};
+  cursor: pointer;
+`;
+
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  height: 32px;
+  border: 1px solid ${PALLETS.LIGHTGRAY};
+  border-left-width: 0;
+  border-right-width: 0;
+  border-top-width: 0;
+  border-bottom-width: 1;
+  font-size: 14px;
+
+  ::placeholder {
+    color: ${PALLETS.LIGHTGRAY};
   }
 
-  input {
-    display: block;
-    width: 100%;
-    height: 32px;
-    border: 1px solid ${PALLETS.LIGHTGRAY};
-    border-left-width: 0;
-    border-right-width: 0;
-    border-top-width: 0;
-    border-bottom-width: 1;
-    font-size: 14px;
-
-    ::placeholder {
-      color: ${PALLETS.LIGHTGRAY};
-    }
-
-    &:focus {
-      outline: none;
-      border-color: ${PALLETS.ORANGE};
-    }
+  &:focus {
+    outline: none;
+    border-color: ${PALLETS.ORANGE};
   }
-  strong {
-    margin-top: 6px;
-    display: none;
-    color: #eb5757;
-    font-size: 12px;
-    line-height: 14px;
+`;
 
-    .error {
-      display: block;
-    }
-  }
+const Error = styled.strong`
+  margin-top: 6px;
+  display: ${(props) => (props.display === 'unavailable' ? 'block' : 'none')};
+  color: #eb5757;
+  font-size: 12px;
+  line-height: 14px;
+`;
 
-  button {
-    display: block;
-    margin-top: 30px;
-    white-space: nowrap;
-    padding: 0px 107px;
-    height: 44px;
-    width: 322px;
-    padding: 13px 0;
-    text-align: center;
-    border-radius: 44px;
+const Errorid = styled.strong`
+  margin-top: 6px;
+  display: ${(props) => (props.display === 'duplication' ? 'block' : 'none')};
+  color: #eb5757;
+  font-size: 12px;
+  line-height: 14px;
+`;
+
+const Btnsubmit = styled.button`
+  margin-top: 30px;
+  white-space: nowrap;
+  padding: 0px 107px;
+  height: 44px;
+  width: 322px;
+  padding: 13px 0;
+  text-align: center;
+  border-radius: 44px;
+  background-color: ${PALLETS.BROWN};
+  color: ${PALLETS.WHITE};
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 17.53px;
+  text-align: center;
+
+  &:disabled {
     background-color: ${PALLETS.BEIGE};
-    color: ${PALLETS.WHITE};
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 17.53px;
-    text-align: center;
+    cursor: not-allowed;
   }
 `;
